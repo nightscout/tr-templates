5fe2bd03ae4d49a2993e3d2c644fabcb895b8b24
Ben West <bewest@gmail.com>
2015-03-13 14:53:58 -0700

    wercker changes the dir names

--- build.sh
+++ build.sh
@@ -17,8 +17,8 @@ ls $OUT
 
 )
 ls -alh
-if [[ -n $WERCKER_GIT_COMMIT ]] ; then
-  cp -rv $WERCKER_OUTPUT_DIR/build/ $OUT/
+if [[ -n $WERCKER_GIT_COMMIT && -d build ]] ; then
+  cp -rv build $OUT
 
 fi
 # bundle install
